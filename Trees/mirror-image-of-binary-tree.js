8cee4f61471e96c6e03b260ed8bc5161fb136901
Amar Jyoti Patra <amarjyotipatra511@gmail.com>
2022-04-14 20:25:10 +0530

    Advance Dsa 2

---
+++ Trees/mirror-image-of-binary-tree.js
@@ -0,0 +1,16 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function mirror(root) {
+  if (root === null) return null;
+  if (root.left !== null) mirror(root.left);
+  if (root.right !== null) mirror(root.right);
+  temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+}
